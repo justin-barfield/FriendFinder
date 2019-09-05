f92a9f01238ab3e4024a0a8b42b6da683387ec96
Justin Barfield <justin.kyle.barfield@gmail.com>
2019-09-04 21:54:37 -0400

    app.post route for api/result added in apiRoutes to calculate the best match available in the db and push the newSubmit to friendsData.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,8 +3,44 @@ const friendsData = require("../data/friends");
 module.exports = function(app) {
 
     app.get("/api/friends", function(req, res) {
+        console.log("Received API request")
         res.json(friendsData);
     });
+
+    app.post("/api/result", function(req, res) {
+
+        var newSubmit = req.body;
+        var newScore = newSubmit.scores;
+
+        var scoreTotals = 0;
+        var perfectMatch = 500;
+
+        var bestMatch = -1;
+
+        for( i =0; i < friendsData.length; i++ ) {
+
+            scoreTotals = 0;
+
+            for( a = 0; a < newScore.length; a++ ) {
+
+                var scoreDifference = Math.abs(newScore[i] - friendsData[i].scores[a]);
+
+                scoreTotals += scoreDifference;
+            }
+
+            if( scoreTotals < perfectMatch ) {
+
+                perfectMatch = scoreTotals;
+                bestMatch = i;
+            }
+
+        }
+        
+        console.log("Match found: " + friendsData[bestMatch]);
+        friendsData.push(newSubmit);
+        res.json(friendsData[bestMatch]);
+
+    })
 
 };
 
